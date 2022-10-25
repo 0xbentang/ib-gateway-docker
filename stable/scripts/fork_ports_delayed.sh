f04c08cd8e0b5d9eb1b6fca82a975c4584745a05
Ben Tang <tangchikin0@gmail.com>
2022-10-25 09:23:02 +0000

    remove unused

--- stable/scripts/fork_ports_delayed.sh
+++ stable/scripts/fork_ports_delayed.sh
@@ -3,9 +3,9 @@
 sleep 30
 
 if [ "$TRADING_MODE" = "paper" ]; then
-  printf "Forking :::4002 onto 0.0.0.0:4000\n"
-  socat TCP-LISTEN:4000,fork TCP:127.0.0.1:4002
+  printf "Forking :::4002 onto 0.0.0.0:$API_PORT\n"
+  socat TCP-LISTEN:$API_PORT,fork TCP:127.0.0.1:4002
 else
-  printf "Forking :::4001 onto 0.0.0.0:4000\n"
-  socat TCP-LISTEN:4000,fork TCP:127.0.0.1:4001
+  printf "Forking :::4001 onto 0.0.0.0:$API_PORT\n"
+  socat TCP-LISTEN:$API_PORT,fork TCP:127.0.0.1:4001
 fi
